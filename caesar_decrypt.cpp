ce0945a00f6f548816eb100ee956d362ef208e4f
25040呂紹謙 <43640574+kenlu250@users.noreply.github.com>
2018-11-29 21:43:31 +0800

    Create caesar_decrypt.cpp

---
+++ caesar_decrypt.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int key = 0;
+  cout << "請輸入密鑰:";
+  cin >> key;
+
+  cout << "請輸入要加密之文件:";
+
+  string word;
+  cin >> word;
+
+  for (int num = 0; num < word[num]; num++){
+    char final_word = word[num] + key;
+    char over = final_word - 26;
+    char other = word[num];
+    if (word[num] >= 65 && word[num] <= 90){
+      if (final_word >= 91){ 
+        cout << over;
+      } 
+      else{
+        cout << final_word;
+      }
+    }
+    else if (word[num] >= 97 && word[num] <= 122){
+      if (final_word >= 123){
+        cout << over;
+      } 
+      else{
+        cout << final_word;
+      }
+    }
+    else{
+      cout << other;
+    }
+  }
+  
+
+}
